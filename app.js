f7cc7c2a85bd6f211e6ff41765c776655fde523e
ibrahim kurut <102996120+ibrahim-kurut@users.noreply.github.com>
2024-04-03 08:32:17 +0300

    Create Error Handler Middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const dbConnection = require('./config/db');
+const { errorHandler, notFoundMiddleware } = require('./middlewares/error');
 
 dotenv.config()
 
@@ -19,6 +20,10 @@ app.use('/api/users', require('./routes/usersRoute'));
 app.use('/api/posts', require('./routes/postsRoute'));
 app.use('/api/comments', require('./routes/commentsRoute'));
 app.use('/api/categories', require('./routes/categoriesRoute'));
+
+// Error Handler Middleware
+app.use(notFoundMiddleware)
+app.use(errorHandler)
 
 // Running Server
 const port = process.env.PORT || 5000;
---
+++ middlewares/error.js
@@ -0,0 +1,22 @@
+// not found middleware
+const notFoundMiddleware = (req, res, next) => {
+    const error = new Error(`Not Found _ ${req.originalUrl}`)
+    error.status = 404;
+    next(error)
+}
+
+
+
+// Error Handler Middleware
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode).json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    });
+}
+
+module.exports = {
+    errorHandler,
+    notFoundMiddleware
+}
